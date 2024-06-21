9c5f05dfa4adc2591073dcfb1d0aa9a1b03cd525
Eric <ericlvalencia@gmail.com>
2024-06-21 14:22:48 -0700

    Switching code over to Udemy course version

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,6 +8,52 @@ const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const log = console.log;
 
+exports.googleSignup = async (req, res) => {
+    const { idToken } = req.body;
+
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken,
+            audience: process.env.GOOGLE_CLIENT_ID,
+        });
+
+        const payload = ticket.getPayload();
+        const { email_verified, name, email } = payload;
+
+        if (email_verified) {
+            let user = await User.findOne({ email });
+            if (user) {
+                const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                const { _id, email, username, role } = user;
+                return res.json({
+                    token,
+                    user: { _id, email, username, role }
+                });
+            } else {
+                let password = email + process.env.JWT_SECRET;
+                user = new User({ username: name, email, password }); // Ensuring username is also set
+                await user.save();
+                const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                const { _id, email, username, role } = user;
+                return res.json({
+                    token,
+                    user: { _id, email, username, role }
+                });
+            }
+        } else {
+            return res.status(400).json({
+                error: 'Google login failed. Try again'
+            });
+        }
+    } catch (error) {
+        console.log('GOOGLE SIGNIN ERROR', error);
+        return res.status(400).json({
+            error: 'Google login failed. Try again'
+        });
+    }
+};
+
+
 exports.checkUsernameAvailability = async (req, res) => {
     const { username } = req.body;
 
@@ -58,7 +104,7 @@ exports.signup = async (req, res) => {
 };
 
 // GOOGLE SIGN UP
-exports.googleSignup = async (req, res) => {
+exports.googleSignup_ = async (req, res) => {
     const { idToken } = req.body;
 
     try {
