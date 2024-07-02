e00302f5be9a3fed1ed7b3a90138bab666d97e5f
Eric <ericlvalencia@gmail.com>
2024-07-02 10:41:44 -0700

    Google account signup - updated backend

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -112,7 +112,7 @@ exports.googleSignup = async (req, res) => {
                 });
             } else {
                 // Create a new user if not found
-                user = new User({ username: payload.name, method: 'google' });
+                user = new User({ username: payload.name, email: payload.email_verified, method: 'google' });
                 await user.save();
                 const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
                 res.cookie('token', token, {
