5c7044523cecb32d90c42090c959e78de360d10b
Eric <ericlvalencia@gmail.com>
2024-07-08 15:32:49 -0700

    Google account signup - updated backend

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const { generateRandomPassword } = require('../utils/password'); // Adjust the path if necessary
-const log = console.log;
+const log = console.log; 
 
 
 const generateUniqueUsername = async (username) => {
