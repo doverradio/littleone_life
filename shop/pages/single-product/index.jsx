9396d200f3b2edd665fe63846e9438623b4af5a5
Eric <ericlvalencia@gmail.com>
2024-09-07 14:18:27 -0700

    Fixed css bug

---
+++ shop/pages/single-product/index.jsx
@@ -0,0 +1,7 @@
+
+import SingleProductPage from "@/components/SingleProductPage";
+
+// Export the component for rendering
+export default function Single_ProductPage() {
+  return <SingleProductPage />;
+}
